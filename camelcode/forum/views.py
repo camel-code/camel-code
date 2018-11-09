d2084cf275372d3e6e6feb557942c2df7abc74a4
Quinn1876 <qhodges0442@gmail.com>
2018-11-08 21:05:58 -0500

    Changed shit

--- camelcode/forum/views.py
+++ camelcode/forum/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
 
 # Create your views here.
 def index(request):
-    return render(request, 'forum/index.html', {'all_albums': 1})
+    return HttpResponse("Hello World.")
 
 def detail(request, post_id):
-    return render(request, 'forum/detail.html', {'all_albums': 1})
+    return render(request, 'forum/detail.html')
