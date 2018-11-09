908e2c0b66d94e6139492de1ffe987251ffc1214
Quinn1876 <qhodges0442@gmail.com>
2018-11-08 21:37:00 -0500

    delete all

--- camelcode/forum/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
-
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello World.")
-
-def detail(request, post_id):
-    return render(request, 'forum/detail.html')
